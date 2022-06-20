9142b30545222cbf3694483ac1fe063360f63a7c
gabrielapl <gxguinho@gmail.com>
2022-06-20 13:29:27 -0300

    .

--- src/components/CardMovieDetails.tsx
+++ src/components/CardMovieDetails.tsx
@@ -1,9 +1,20 @@
-import { View } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export function CardMovieDetails() {
   return (
     <View>
-      
+      <Image source={{ uri: 'https://image.tmdb.org/t/p/original/yFSIUVTCvgYrpalUktulvk3Gi5Y.jpg' }} style={styles.poster} />
+      <View>
+        <Text>Vingadores</Text>
+        <Text>Assistido</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  poster: {
+    height: 220,
+    width: 180
+  }
+});
